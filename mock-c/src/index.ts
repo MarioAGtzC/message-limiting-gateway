5bcc9a43a244380bd0a9da65606375602c9c6382
Mario Gutierrez <marioagtzc@hotmail.com>
2026-04-07 01:06:20 -0600

    Mock C Docker File and limiter

--- mock-c/src/index.ts
+++ mock-c/src/index.ts
@@ -2,19 +2,20 @@ import express from 'express';
 import rateLimit from 'express-rate-limit';
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
-// const limiter = rateLimit({
-//   windowMs: 1000, // 1 segundo
-//   max: 10, // 10 peticiones por segundo
-//   statusCode: 429,
-//   message: { error: 'Rate limit exceeded' },
-// });
+const limiter = rateLimit({
+  windowMs: 1000,
+  max: 100,
+  statusCode: 429,
+  message: { error: 'Rate limit exceeded' },
+});
 
-app.post('/send-message', (req, res) => {
+app.post('/send-message', limiter, (req, res) => {
   res.status(200).json({ success: true })
 });
 
-app.listen(3001, () => {
-  console.log('Mock C corriendo en puerto 3001')
+app.listen(PORT, () => {
+  console.log(`Mock C corriendo en puerto ${PORT}`)
 });
